d6637f4f8d70977f448917d805aec2f4c914fe09
João <desenvolvimento@orions.com.br>
2020-11-18 15:25:20 -0300

    recreated json and new treatment

--- index.js
+++ index.js
@@ -9,53 +9,29 @@ fs.readFile('./tabela.json', function read(err, data) {
     processFile(content);
 });
 
-function processFile(content) {
-    const json = JSON.parse(content);
+const processFile = (content) => {
+    const json = JSON.parse(content)[0];
+    var count = 0;
 
-    let output = "INSERT INTO oc_setting (store_id, `code`, `key`, `value`, serialized) VALUES";
-    var aux = {};
+    let output = `INSERT INTO oc_faixas_rodonaves (cidade, valor, cep_inicial, cep_final, peso_minimo, peso_maximo, tempo_estimado) VALUES`;
 
-    const code = 'shipping_frete_customizado';
-    const key = 'shipping_frete_customizado_fretes';
+    console.log("Starting JSON iteration...");
 
-    console.log("Starting JSON iteration...")
-
-    var old_value = '';
-
-    json.forEach((chunk) => {
-    	chunk.forEach((element) => {
-    		let value;
-    		if(typeof element.Cidade !== 'undefined'){
-    			aux.title = ""+element['Cidade'].replace('\'','')+"";
-    			aux.total = ""+element['Frete'].toFixed(2)+"";
-    			aux.minimum = "";
-    			aux.start_zipcode = ""+element['CEP Inicial']+"";
-    			aux.end_zipcode = ""+element['CEP Final']+"";
-    			aux.start_weight = ""+element['Peso Mínimo']+"";
-    			aux.end_weight = ""+element['Peso Máximo']+"";
-    			aux.tempo_estimado = ""+element['Lead Time']+"";
-    		}
-
-    		value = '[' + JSON.stringify(aux) + ']';
-
-            if(value !== old_value){
-                output += `\n (0, '${code}', '${key}', '${value}', 1),`
-                old_value = '';
-            }
-            
-            old_value = value;
-            
-    	})
+    json.forEach((res) => {
+        res.valor = res.valor.toFixed(2);
+        res.cidade = res.cidade.replace('\'', '\\\'');
+        output += `\n('${res.cidade}', ${res.valor}, '${res.cep_inicial}', '${res.cep_final}', ${res.peso_minimo}, ${res.peso_maximo}, ${res.tempo_estimado}),`;          
+        count++;
     })
 
     console.log("Finished JSON iteration.");
 
-    console.log('Starting writing SQL file...')
+    console.log('Starting writing SQL file...');
 
-    output = output.replace(/.$/,";")
+    output += output.slice(0, -1) + ';';
 
     fs.writeFile('fretes.sql', output, (err) => {
       if (err) return console.error('\x1b[31m%s\x1b[0m',err);
-      console.log('\x1b[32m%s\x1b[0m',"\nSQL file successfully created.");
+      console.log('\x1b[32m%s\x1b[0m',`\nSQL file successfully created. ${count} rows.`);
     });
 }
